88020ab9497625147255386327c8e1bbb42baa22
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-25 21:24:41 +0100

    Document thread monitors

--- src/main/java/net/openhft/chronicle/threads/ThreadMonitor.java
+++ src/main/java/net/openhft/chronicle/threads/ThreadMonitor.java
@@ -22,7 +22,15 @@ import net.openhft.chronicle.core.threads.EventHandler;
 import net.openhft.chronicle.core.threads.HandlerPriority;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Event handler used by the monitor loop to detect threads that appear to be
+ * blocked. Instances are typically produced by {@link ThreadMonitors}.
+ */
 public interface ThreadMonitor extends EventHandler {
+    /**
+     * Returns {@link HandlerPriority#MONITOR} so monitoring does not compete
+     * with application handlers.
+     */
     @Override
     default @NotNull HandlerPriority priority() {
         return HandlerPriority.MONITOR;
