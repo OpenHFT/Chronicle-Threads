fa79293acb1c598f717f49294c3266c74d6912c2
Peter Lawrey <peter.lawrey@chronicle.software>
2025-05-25 21:44:30 +0100

    Add Javadoc for EventHandlers enum (#301)

--- src/main/java/net/openhft/chronicle/threads/EventHandlers.java
+++ src/main/java/net/openhft/chronicle/threads/EventHandlers.java
@@ -20,6 +20,11 @@ package net.openhft.chronicle.threads;
 
 import net.openhft.chronicle.core.threads.EventHandler;
 
+/**
+ * Placeholder enum that holds simple {@link EventHandler} constants.
+ * The only entry is {@link #NOOP}, whose {@code action()} method always
+ * returns {@code false}.
+ */
 enum EventHandlers implements EventHandler {
     NOOP {
         @Override
