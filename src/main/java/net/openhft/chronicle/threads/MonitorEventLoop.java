037b6dc828548cfbccccf6416e876ab7bf3ae784
Peter Lawrey <peter.lawrey@chronicle.software>
2025-09-10 15:21:18 +0100

    Refactor event loop classes to change privateGroup visibility and update handler implementation

--- src/main/java/net/openhft/chronicle/threads/MonitorEventLoop.java
+++ src/main/java/net/openhft/chronicle/threads/MonitorEventLoop.java
@@ -19,8 +19,8 @@ package net.openhft.chronicle.threads;
 
 import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.annotation.HotMethod;
-import net.openhft.chronicle.core.io.AbstractCloseable;
 import net.openhft.chronicle.core.io.Closeable;
+import net.openhft.chronicle.core.io.SimpleCloseable;
 import net.openhft.chronicle.core.threads.EventHandler;
 import net.openhft.chronicle.core.threads.EventLoop;
 import net.openhft.chronicle.core.threads.HandlerPriority;
@@ -180,7 +180,7 @@ public class MonitorEventLoop extends AbstractLifecycleEventLoop implements Runn
      * easy way to achieve that is to wrap the handler in this idempotent decorator and
      * call it at the start of every iteration.
      */
-    private static final class IdempotentLoopStartedEventHandler extends AbstractCloseable implements EventHandler {
+    private static final class IdempotentLoopStartedEventHandler extends SimpleCloseable implements EventHandler {
 
         private transient final EventHandler eventHandler;
         private final String handler;
